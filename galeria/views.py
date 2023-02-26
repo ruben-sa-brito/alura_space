e183caabd3f1ea26edd48d7dd6687b1af602806e
Timewound <rubenjms1@gmail.com>
2023-02-26 11:45:43 -0300

    incluindo link para imagens

--- galeria/views.py
+++ galeria/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'galeria/index.html')
 
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
+
 # Create your views here.

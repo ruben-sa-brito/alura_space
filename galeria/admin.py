c6789eb829a212ed26d4e5bd1295ee0bfacf582b
Timewound <rubenjms1@gmail.com>
2023-03-07 20:50:45 -0300

    trabalhando com django admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id','nome','legenda')
+    
+admin.site.register(Fotografia, ListandoFotografias)    

49d146df90fc0dc6d82de48912b3bef0e8dc80d2
Timewound <rubenjms1@gmail.com>
2023-03-09 20:07:14 -0300

    incluindo categoria ao model Fotografia,filtros e list_per_page

--- galeria/models.py
+++ galeria/models.py
@@ -3,8 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+    
+    OPCOES_CATEGORA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+    
+    
     nome = models.CharField(max_length=100, null = False, blank = False)
     legenda = models.CharField(max_length=150, null=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORA, default='')
     descricao = models.TextField(null = False, blank=False)
     foto = models.CharField(max_length=100, null = False, blank = False)
     

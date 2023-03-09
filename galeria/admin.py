e7c4bbfdb8b9e2afb6db1779692c3b838455b172
Timewound <rubenjms1@gmail.com>
2023-03-09 20:32:16 -0300

    incluindo BooleanField: publicada

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id','nome','legenda')
+    list_display = ('id','nome','legenda', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome', )
     list_filter = ('categoria', )
     list_per_page = 10
+    list_editable = ('publicada', )
     
 admin.site.register(Fotografia, ListandoFotografias)    

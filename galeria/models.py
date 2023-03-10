a1576687fbcbb6dfd889af63427b7e637ec42b87
Timewound <rubenjms1@gmail.com>
2023-03-09 21:56:27 -0300

    upload de imagens

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Fotografia(models.Model):
@@ -16,8 +16,9 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORA, default='')
     descricao = models.TextField(null = False, blank=False)
-    foto = models.CharField(max_length=100, null = False, blank = False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank = True)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     
     def __str__(self):
         return f"Fotografia [nome = {self.nome}]"

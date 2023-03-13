8c2fb19cba7ddb5a4bd67447722fcff7f756fb35
Timewound <rubenjms1@gmail.com>
2023-03-13 20:11:37 -0300

    adicionando novo app usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, "usuarios/login.html")
+
+
+def cadastro(request):
+    return render(request, "usuarios/cadastro.html")
